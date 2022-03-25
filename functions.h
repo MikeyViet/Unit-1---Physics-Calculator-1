00bc0780a18d346983bfffd0b68591bd7e5c627e
Michael_Nguyen Nguyen <98675293+MikeyViet@users.noreply.github.com>
2022-03-25 06:52:34 +0000

    Created display Menu Function

--- functions.h
+++ functions.h
@@ -7,14 +7,34 @@
 *  Teacher:    Tyson McMillan
 *  Date:       03/25/2022
 *********************************************************************/
-
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+
+#include<iostream>
+#include<string>
+#include"Input_Validation_Extended.h"
 
 /********************************************************************
 *                      FUNCTION PROTOTYPES
 *********************************************************************/
 
+void displayMenu();
+
+
 /********************************************************************
 *                      FUNCTION DEFINITIONS
 *********************************************************************/
+
+//This function displays the menu on screen for user
+void displayMenu()
+{
+  cout << "Please select one of the following calculations to perform by entering one of the alphabetical letter that precedes it: "<< endl << endl;
+  cout << "(A) Velocity" << endl;
+  cout << "(B) Acceleration" << endl;
+  cout << "(C) Motion" << endl;
+  cout << "(D) Force - Newton's Second Law" << endl;
+  cout << "(E) Weight (planet Earth)" << endl;
+  cout << "(F) Momentum" << endl;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
-#include <iostream>
+/********************************************************************
+*  Assignment: Unit 1
+*  Title:      Physics Calculator
+*  Student:    Michael Nguyen
+*  Teacher:    Tyson McMillan
+*  Date:       03/25/2022
+*
+*  Description: This is a program that will mimic a physics 
+*    calculator.  The program will display a menu with several 
+*    options to calculate several physics formulas. 
+*    
+*********************************************************************/
+#include "functions.h"
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  displayMenu();
+
+  return 0;
 }
